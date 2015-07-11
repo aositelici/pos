9b109f8854cf83b236af141b160e462ae8f40bf4
liya <liya013@live.com>
2015-07-11 14:28:01 +0800

    finished posv1_3rd & modified posv1

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -1,13 +1,13 @@
 function reWriteInput(inputs) {
   var count = {};
-  for(var i = 0;i < inputs.length; i ++){
-    if(inputs[i].indexOf('-') !== -1){
-      count[inputs[i].slice(0,inputs[i].length-2)] = parseInt(inputs[i].slice(-1));
+  inputs.forEach(function(input){
+    if(input.indexOf('-') !== -1){
+      count[input.slice(0,input.length-2)] = parseInt(input.slice(-1));
     }
     else{
-      count[inputs[i]] = count[inputs[i]] + 1 || 1;
+      count[input] = count[input] + 1 || 1;
     }
-  }
+  });
   var result =[];
   for(var bar in count){
     result.push({barcode:bar,count:count[bar]});
@@ -33,41 +33,50 @@ function getPrintItem(input,all,sale){
       }
     }
   });
-  for(var i = 0; i< printItem.length; i ++){
-    printItem[i].count = input[i].count;
-    printItem[i] = getCount(printItem[i],sale);
-  }
+  var i = 0;
+  printItem.forEach(function(item){
+    item.count = input[i++].count;
+    getCount(item,sale);
+  });
   return printItem;
 }
-function printDetail(inputs) {
-  var item = ['名称：', '数量：', '单价：', '小计：'];
-  var unit = '(元)';
+function partText(input){
+  var price = (input.count - input.countSale) * input.price;
+  var salePrice = input.countSale * input.price;
+  var text = '名称：' + input.name + '，' + '数量：' + input.count + input.unit + '，' + '单价：' +
+    input.price.toFixed(2) + '(元)' + '，' + '小计：' + price.toFixed(2) + '(元)' + '\n';
+  return {text: text, price: price, saleprice: salePrice};
+}
+function printDetail1(inputs) {
   var text = '***<没钱赚商店>收据***\n';
   var totalPrice = 0;
   var totalSalePrice = 0;
-  for (var i = 0; i < inputs.length; i++) {
-    var price = (inputs[i].count - inputs[i].countSale) * inputs[i].price;
-    var salePrice = inputs[i].countSale * inputs[i].price;
-    totalPrice += price;
-    totalSalePrice += salePrice;
-    text += item[0] + inputs[i].name + '，' + item[1] + inputs[i].count + inputs[i].unit + '，' + item[2] +
-      inputs[i].price.toFixed(2) + unit + '，' + item[3] + price.toFixed(2) + unit + '\n';
-  }
+  inputs.forEach(function(input){
+    totalPrice += partText(input).price;
+    totalSalePrice += partText(input).saleprice;
+    text += partText(input).text;
+  });
   return {text: text, totalprice: totalPrice, totalsaleprice: totalSalePrice};
 }
-function print(inputs){
-  var item=['名称：','数量：','单价：','小计：'];
-  var unit='(元)';
-  var detail = printDetail(inputs);
-  detail.text += '----------------------\n' + '挥泪赠送商品：\n';
-  for(var i = 0; i < inputs.length; i ++) {
-    if(inputs[i].countSale != 0){
-      detail.text += item[0] + inputs[i].name + '，' + item[1] + inputs[i].countSale + inputs[i].unit + '\n';
+function printDetail2(inputs) {
+  var text = '';
+  inputs.forEach(function(input){
+    if(input.countSale != 0){
+      text += '名称：' + input.name + '，' + '数量：' + input.countSale + input.unit + '\n';
     }
-  }
-  detail.text += '----------------------\n'  + '总计：' +detail.totalprice.toFixed(2) + unit+ '\n' +'节省：'+detail.totalsaleprice.toFixed(2) +
-  unit +'\n'+'**********************';
-  return detail.text;
+  });
+  return text;
+}
+function print(inputs){
+  var detail = printDetail1(inputs);
+  var text = detail.text;
+  text += '----------------------\n'
+    + '挥泪赠送商品：\n';
+  text += printDetail2(inputs);
+  text += '----------------------\n'
+    + '总计：' +detail.totalprice.toFixed(2) + '(元)'+ '\n' +'节省：' +detail.totalsaleprice.toFixed(2) + '(元)'
+    +'\n'+'**********************';
+  return text;
 }
 function printReceipt(inputs) {
   var inputItem = reWriteInput(inputs);
--- pos_v1_3rdlib/main/main.js
+++ pos_v1_3rdlib/main/main.js
@@ -1,1 +1,80 @@
-//TODO: Please write code in this file.
+function reWriteInput(inputs) {
+  var count = {};
+  inputs.forEach(function(input){
+    if(input.indexOf('-') !== -1){
+      count[input.slice(0,input.length-2)] = parseInt(input.slice(-1));
+    }
+    else{
+      count[input] = count[input] + 1 || 1;
+    }
+  });
+  var result =[];
+  for(var bar in count){
+    result.push({barcode:bar,count:count[bar]});
+  }
+  return result;
+}
+function getCount(printItem,sale){
+  printItem.countSale = 0;
+  if(_.pluck(sale, 'barcodes').indexOf(printItem.barcode)){
+    printItem.countSale = parseInt(printItem.count/3);
+  }
+}
+function getPrintItem(input,all,sale){
+  var printItem = all.filter(function(item){
+    return ((_.pluck(input, 'barcode')).indexOf(item.barcode) !== -1);
+  });
+  var i = 0;
+  printItem.forEach(function(item){
+    item.count = input[i++].count;
+    getCount(item,sale);
+  });
+  return printItem;
+}
+function partText(input){
+  var price = (input.count - input.countSale) * input.price;
+  var salePrice = input.countSale * input.price;
+  var text = '名称：' + input.name + '，' + '数量：' + input.count + input.unit + '，' + '单价：' +
+    input.price.toFixed(2) + '(元)' + '，' + '小计：' + price.toFixed(2) + '(元)' + '\n';
+  return {text: text, price: price, saleprice: salePrice};
+}
+function printDetail1(inputs) {
+  var text = '***<没钱赚商店>收据***\n';
+  var totalPrice = 0;
+  var totalSalePrice = 0;
+  inputs.forEach(function(input){
+    totalPrice += partText(input).price;
+    totalSalePrice += partText(input).saleprice;
+    text += partText(input).text;
+  });
+  return {text: text, totalprice: totalPrice, totalsaleprice: totalSalePrice};
+}
+function printDetail2(inputs) {
+  var text = '';
+  inputs.forEach(function(input){
+    if(input.countSale != 0){
+      text += '名称：' + input.name + '，' + '数量：' + input.countSale + input.unit + '\n';
+    }
+  });
+  return text;
+}
+function print(inputs){
+  var detail = printDetail1(inputs);
+  var text = detail.text;
+  text += '----------------------\n'
+    + '挥泪赠送商品：\n';
+  text += printDetail2(inputs);
+  text += '----------------------\n'
+    + '总计：' +detail.totalprice.toFixed(2) + '(元)'+ '\n' +'节省：' +detail.totalsaleprice.toFixed(2) + '(元)'
+    +'\n'+'**********************';
+  return text;
+}
+function printReceipt(inputs) {
+  var inputItem = reWriteInput(inputs);
+  var allItem = loadAllItems();
+  var saleItem = loadPromotions()[0];
+  var findItem = getPrintItem(inputItem,allItem,saleItem);
+  var text = print(findItem);
+  console.log(text);
+}
+
