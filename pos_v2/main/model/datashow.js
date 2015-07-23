edb1151c7dd2705e135219f08ec2f10e846e83fb
liya <liya013@live.com>
2015-07-23 10:43:11 +0800

    add datashow.js

---
+++ pos_v2/main/model/datashow.js
@@ -0,0 +1,14 @@
+function DataShow(currentDate) {
+  this.year = this.dateDigitToString(currentDate.getFullYear());
+  this.month = this.dateDigitToString(currentDate.getMonth() + 1);
+  this.date = this.dateDigitToString(currentDate.getDate());
+  this.hour = this.dateDigitToString(currentDate.getHours());
+  this.minute = this.dateDigitToString(currentDate.getMinutes());
+  this.second = this.dateDigitToString(currentDate.getSeconds());
+  this.formattedDateString = this.year + '年' + this.month + '月' + this.date + '日 '
+    + this.hour + ':' + this.minute + ':' + this.second;
+}
+
+DataShow.prototype.dateDigitToString = function (num) {
+  return num < 10 ? '0' + num : num;
+};
